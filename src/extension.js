901ace9eb7d50a6b58334b3c38148a3ce334c2e8
lanly_dev <lanly-dev@users.noreply.github.com>
2019-01-27 11:06:20 -0800

    src folder

--- src/extension.js
+++ src/extension.js
@@ -3,7 +3,9 @@ const vscode = require('vscode')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	vscode.window.showInformationMessage('Hello World!')
+  vscode.window.showInformationMessage('Hello World!')
+  let files = vscode.workspace.findFiles('**/*.mp3', null, 100)
+  files.then((data) => console.log(data),(error) => console.error(error))
 }
 exports.activate = activate
 
