0ee90e04b682f68b4c3287030bc87bc5c8636e09
lanly_dev <lanly-dev@users.noreply.github.com>
2019-11-23 23:06:49 -0800

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 module.exports = {
-    mode: 'production',
-    entry: "./src/controller.js",
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, 'src')
-    }
+  mode: 'production',
+  entry: "./src/controller.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname,'src')
   }
+}
