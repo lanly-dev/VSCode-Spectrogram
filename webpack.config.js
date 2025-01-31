d5fc2879ff29904342319f1b72d4bcc1c652fcd1
Lan Ly <lanly-dev@users.noreply.github.com>
2025-01-31 00:15:53 -0800

    config: wp

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,37 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 const path = require('path')
 const CODICON_PATH = '@vscode/codicons/dist/'
-const isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  target: 'node',
-  mode: 'none',
-  devtool: 'inline-cheap-source-map',
-  entry: './src/extension.js',
-  output: {
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2', // important
-    path: path.resolve(__dirname, 'dist')
-  },
-  externals: {
-    vscode: 'commonjs vscode'
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: require.resolve(`${CODICON_PATH}/codicon.css`) },
-        { from: require.resolve(`${CODICON_PATH}/codicon.ttf`) },
-        { from: 'src/controller.js' },
-        { from: 'src/index.pug' },
-        { from: 'src/style.css' }
-      ]
-    })
-  ],
-  optimization: {
-    minimize: isProduction,
-    minimizer: isProduction ? [new TerserPlugin({ extractComments: false }), new CssMinimizerPlugin()] : []
+module.exports = (env, argv) => {
+  const { devtool, mode} = argv
+  console.log('🥼🥼', 'MODE -->', mode, '🥼🥼')
+  return {
+    target: 'node',
+    mode,
+    devtool,
+    entry: './src/extension.js',
+    output: {
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2', // important
+      path: path.resolve(__dirname, 'dist')
+    },
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: require.resolve(`${CODICON_PATH}/codicon.css`) },
+          { from: require.resolve(`${CODICON_PATH}/codicon.ttf`) },
+          { from: 'src/controller.js' },
+          { from: 'src/index.pug' },
+          { from: 'src/style.css' }
+        ]
+      })
+    ],
+    optimization: {
+      minimize: mode === 'production',
+      minimizer: mode === 'production' ? [new TerserPlugin({ extractComments: false }), new CssMinimizerPlugin()] : []
+    }
   }
 }
